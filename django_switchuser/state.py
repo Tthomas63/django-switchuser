eb8b71622201f66ef1862a4976c12ef83145b5e8
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-03-29 13:17:43 -0700

    Hasattr checks for get_session_auth_hash (Django compat)

--- django_switchuser/state.py
+++ django_switchuser/state.py
@@ -85,7 +85,8 @@ class SuState(object):
     def set_su_user(self, su_user):
         """ Switches to user ``su_user`` without permissions checks. """
         self.request.session[AUTH_SESSION_KEY] = su_user.id
-        self.request.session[AUTH_HASH_SESSION_KEY] = su_user.get_session_auth_hash()
+        if hasattr(su_user, 'get_session_auth_hash'):
+            self.request.session[AUTH_HASH_SESSION_KEY] = su_user.get_session_auth_hash()
         if su_user.id != self.auth_user.id:
             self.request.session["su_auth_user_id"] = self.auth_user.id
         else:
@@ -94,5 +95,6 @@ class SuState(object):
         self.request.user = su_user
 
     def clear_su(self):
-        self.request.session[AUTH_HASH_SESSION_KEY] = self.auth_user.get_session_auth_hash()
+        if hasattr(self.auth_user, 'get_session_auth_hash'):
+            self.request.session[AUTH_HASH_SESSION_KEY] = self.auth_user.get_session_auth_hash()
         self.set_su_user(self.auth_user)
